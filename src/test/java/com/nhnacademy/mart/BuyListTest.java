38f26750563a9c24b961f027e1af47f8681a71b2
heen5620 <heen5620@naver.com>
2023-09-02 23:21:04 +0900

    initial commit

---
+++ src/test/java/com/nhnacademy/mart/BuyListTest.java
@@ -0,0 +1,22 @@
+package com.nhnacademy.mart;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BuyListTest {
+
+    @Test
+    @DisplayName("올바른 값이 buylist안에 있는지 확인")
+    void buylistTest(){
+        BuyList buyList = new BuyList();
+        BuyList.Item item = new BuyList.Item("참치", 2);
+        buyList.add(item);
+        Assertions.assertEquals("참치", buyList.getItems().get(0).getName());
+        Assertions.assertEquals(2, buyList.getItems().get(0).getAmount());
+    }
+
+
+}
---
+++ src/test/java/com/nhnacademy/mart/FoodStandTest.java
@@ -0,0 +1,38 @@
+package com.nhnacademy.mart;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class FoodStandTest {
+
+    /*
+    1. Food가 잘 들어가는지
+    2. food를 추가하고 잘 제거 되는지 테스트
+     */
+    @Test
+    @DisplayName("정상 추가되는지 확인")
+    void correctTest() {
+        FoodStand foodStand = new FoodStand();
+        Food food = new Food("참치", 2000);
+        foodStand.add(food);
+
+        Assertions.assertTrue(foodStand.getFoodsName("참치"));
+    }
+
+    @Test
+    @DisplayName("제거 테스트")
+    void removeTesdt() {
+        FoodStand foodStand = new FoodStand();
+        Food food1 = new Food("참치", 2000);
+        Food food2 = new Food("고등어", 1000);
+        foodStand.add(food1);
+        foodStand.add(food2);
+
+        foodStand.removeFood("참치");
+        Assertions.assertEquals(1, foodStand.getFood().size());
+
+    }
+}
